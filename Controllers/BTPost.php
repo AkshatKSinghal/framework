5794940e117961295ddcb17a1cd8366b78e9355b
akshat <akshat@browntape.com>
2017-04-27 17:42:27 +0530

    Courier Company classes

--- Controllers/BTPost.php
+++ Controllers/BTPost.php
@@ -7,24 +7,92 @@
 
 class BTPost
 {
-	public static function uploadAWB($filePath, $accountId, $courierCompanyID)
+	private var $accountID;
+
+	function __construct($accountID)
 	{
-		// Check courier company ID, throw exception if invalid
+		$this->accountID = $accountID;
 	}
 
-	private static function basicValidateFile($filePath)
+
+	/**
+	 * Function to upload AWB batch into a courier company
+	 * 
+	 * @param string $filePath Path of file on the disk
+	 * 
+	 * @return mixed $response AWB Batch Number if file is valid
+	 * Error message in case the file has errors
+	 * Error message in case Courier Company ID is invalid
+	 */
+	public function uploadAWB($filePath, $courierCompanyID)
 	{
-		//Check for duplicates, throw exception in case duplicates within file are found
+		// Check courier company ID, return error if invalid
 	}
 
+
+	/**
+	 * Function to perform basic validations on the file
+	 * i.e. Duplicates within the file, Invalid Characters, Empty lines etc
+	 * 
+	 * @param string $filePath Path of the file on disk
+	 * 
+	 * @throws Exception in case the file contains duplicates or invalid characters
+	 * 
+	 * @return void
+	 */
+	private function basicValidateFile($filePath)
+	{
+		#TODO Check for duplicates, throw exception in case duplicates within file are found
+	}
+
+
+	#TODO Put complete list of required parameters
 	/**
 	 * Function to create a new courier company
 	 * 
 	 * @param string $name Name of the courier company
 	 * @param 
+	 * 
+	 * @return mixed $response Courier company information in case the creation is successful
+	 * Error message in case the operation is unsuccessful
 	 */
-	public static function createCourierCompany($name, )
+	public function createCourierCompany($name, $shortCode, $comments, $logoURL)
 	{
 
+	}
+
+	public function getAccountId()
+	{
+		return $this->accountID;
+	}
+
+
+	/**
+	 * Function to track shipment via BTPost Reference ID
+	 * 
+	 * @param string $refId BTPost Reference ID
+	 * 
+	 * @return mixed $response Tracking information as returned from trackShipmentViaAWB
+	 * @see trackShipmentViaAWB
+	 */
+	public function trackShipment($refId)
+	{
+		#TODO check if the reference number belongs to the account
+		#TODO get AWB and Courier Company ID
+		return self::trackShipmentViaAWB($awb, $courierCompanyID);
+	}
+
+	/**
+	 * API to track the AWB for given courier company
+	 * 
+	 * @param string $awb AWB to be tracked
+	 * @param string $courierCompanyID ID of the courier company
+	 * 
+	 * @return mixed $reponse Tracking information for the AWB number 
+	 */
+	private function trackShipmentViaAWB($awb, $courierCompanyID)
+	{
+		#TODO Create courierCompany instance
+		return $courierCompany->trackShipment($awb);
 	}
 }
--- Controllers/CourierService.php
+++ Controllers/CourierService.php
@@ -13,9 +13,67 @@ class CourierService extends CourierCompany
 	 * @return bool $allowed based on the settings saved for the 
 	 * courier service and courier company
 	 */
+
 	public function preallocateAWBAllowed()
 	{
-		// return value based on the DB config && CourierCompany->preallocateAWBAllowed();
+		return $this->model->getSettings('AWBUpload');
 	}
 
+
+	/**
+	 * Function to handle bookShipment, schedulePickup and tracking requests
+	 * 
+	 * @throws Exception in case the required service is not known
+	 * @throws Exception in case the required service is not supported/ integrated 
+	 * for the courier
+	 * 
+	 * @return mixed $response Response as per the function called
+	 */
+	public function __call($function, $arguments)
+	{
+		if (!method_exists($this, $function)) {
+			throw new Exception("Invalid Operation");
+		}
+
+		#TODO Determine the courier company class
+		$this->checkService($courierCompanyClass, $function);
+		return $this->$function($arguments[0]);
+	}
+
+	/**
+	 * Function to book shipment for the order via the given courier service
+	 * 
+	 * @param mixed $orderInfo Array containing order(shipment) information
+	 * 
+	 * @throws Exception in case the order information is invalid/ incomplete
+	 * @throws Exception in case the Tracking for Courier Service is not available
+	 * @throws Exception in case the Courier API gives unknown response/ error
+	 * 
+	 * @return string $awb AWB number allocated by the courier service
+	 */
+	protected function bookShipment($orderInfo)
+	{
+		$this->checkService($courierCompanyClass, "bookShipment");
+	}
+
+
+	/**
+	 * Function to check if the service is supported by the courier and is enabled
+	 * 
+	 * @param string $service Service being requested i.e. 
+	 * bookShipment, schedulePickup, tracking etc
+	 * 
+	 * @throws Exception in case the service is not supported/ integrated 
+	 * for the courier
+	 * 
+	 * @return void
+	 */
+	public function checkService($service)
+	{
+		#TODO get $courierCompanyClass,
+		if (!class_exists($courierCompanyClass) 
+			|| !method_exists($courierCompanyClass, $service)) {
+			throw new Exception("$service service not available for the courier");
+		}
+	}
 }
