80020765bae46dead55324389144cbef0602bd15
akshat <akshat@browntape.com>
2017-05-04 19:39:36 +0530

    Revert "Fetching all fields"
    This reverts commit 3e3e30c06ec8b332dbf97a1cc78f3563138c0b38.

--- Model/AWBBatch.php
+++ Model/AWBBatch.php
@@ -17,7 +17,7 @@ class AWBBatch extends \Model\Base
             'courier_company_id' => $this->get('courierCompanyID'),
             'account_id' => $this->get('accountID'),
             );
-        $response = DBManager::search($this->tableName(), $searchCondition);
+        $response = DBManager::search($this->tableName(), $searchCondition, array($this->primaryKey()));
         $data = [];
         while($row = $response->fetch_assoc()) {
             $data[] = $row[$this->primaryKey()];
