cf8f996dc5254fd335cb862f0bd0a74a20210f78
archit <archit@browntape.com>
2017-06-21 15:22:27 +0530

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    '\DB\DB' => [
+        "hostname" => "localhost",
+        "username" => "root",
+        "password" => "password",
+        "database" => "db"
+    ],
+    '\Cache\CacheManager' => [
+        'host' => '127.0.01',
+        'port' => 6379
+    ]
+];
